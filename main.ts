adae66a1a906c232d14146034989cf4543f014ae
Ruirodri03 <122972465+Ruirodri03@users.noreply.github.com>
2023-01-18 09:57:09 +0000

    Ficheiros iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,53 @@
+let Numero = 0
+input.onGesture(Gesture.Shake, function () {
+    Numero = randint(1, 6)
+    if (Numero == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (Numero == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (Numero == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (Numero == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (Numero == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (Numero == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
